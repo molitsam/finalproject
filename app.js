3d3b1ef2b7a34abe8d7d141a4bdebdc3ae6de2f1
Sade Molitor <500169348@fvtc.edu>
2024-05-07 16:55:28 -0500

    mongo

--- app.js
+++ app.js
@@ -2,5 +2,19 @@
 //Back end Data Base information //LADIES: Dont Forget Notes S
 
 // Expess File requiremnet 
-const express = require('expresss')
+const express = require('express')
+const path = require('path')
+const mongo = require('./mongo')
+
+const app = express()
+
+const root = path.join(__dirname, 'public')
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (_, response) => {
+    response.sendFile('home.html', { root })
+})
 
